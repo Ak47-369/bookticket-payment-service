5db5147c76ea8cf4b5ee025dc937681ceb06c823
Amit Yadav <akydv2627@gmail.com>
2025-11-13 23:52:48 +0530

    init commit

---
+++ src/test/java/com/bookticket/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookticket.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
